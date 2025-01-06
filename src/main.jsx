6267e09976ee711397d50846fa24b8a4249d6287
Sivakumar <sivakumarp132003@gmail.com>
2025-01-06 11:11:46 +0530

    Third comit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import ButtonMain from './pages/Home/ButtonMain.jsx'
-import StoreContext from './page2/StoreContext.jsx'
-import ToggleTheme from './components/ToggleTheme/toggleTheme.jsx'
 import Apps from './Apps.jsx'
 
 createRoot(document.getElementById('root')).render(
